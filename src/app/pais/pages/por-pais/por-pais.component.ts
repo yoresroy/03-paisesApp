eae0486909ccddae2d6e1a9811db2bd02b671bcb
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2022-09-27 18:18:39 -0500

    add - Tabla para manejo de paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './por-pais.component.html',
   styleUrls: ['./por-pais.component.css']
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+
+  termino: string = '';
 
   constructor() { }
+ 
 
-  ngOnInit(): void {
+  buscar() {
+    console.log(this.termino);
   }
 
 }
