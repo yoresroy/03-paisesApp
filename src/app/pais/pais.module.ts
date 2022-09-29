ebce41b209b4db45833093e9ebdfafaa81fbe1ea
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2022-09-29 18:25:48 -0500

    Se separa tabla como componente

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -7,20 +7,22 @@ import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
 import { RouterModule } from '@angular/router';
-
+import { PaisTablaComponent } from './components/pais-tabla/pais-tabla.component';
 
 @NgModule({
   declarations: [
     PorCapitalComponent,
     PorPaisComponent,
     PorRegionComponent,
-    VerPaisComponent
+    VerPaisComponent,
+    PaisTablaComponent
   ],
   exports:[
     PorCapitalComponent,
     PorPaisComponent,
     PorRegionComponent,
-    VerPaisComponent
+    VerPaisComponent,
+    PaisTablaComponent
   ],
   imports: [
     CommonModule,
