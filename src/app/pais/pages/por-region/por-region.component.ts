86c814ae03b30ea2333f787865a49dd5bafc0819
RodrigoMegasys <rodrigo.ruiz@megasys.com.mx>
2022-10-04 18:29:57 -0500

    WIP - Inicio de busqueda por región

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorRegionComponent implements OnInit {
 
+  regiones:string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+
   constructor() { }
 
   ngOnInit(): void {
